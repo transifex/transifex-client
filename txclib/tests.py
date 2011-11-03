a6f525824ad28bcb526e3709167b2d1c376f583e
Apostolos Bessas <mpessas@indifex.com>
2011-11-03 18:05:58 +0200

    Add tests for the filtering functions.

--- txclib/tests.py
+++ txclib/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
+from __future__ import with_statement
 import unittest
 import itertools
 from mock import patch
@@ -30,7 +30,6 @@ class TestProjectMinimumPercent(unittest.TestCase):
         self.p = Project(init=False)
         self.p.minimum_perc = None
         self.p.resource = "resource"
-
 
     def test_cmd_option(self):
         """Test command-line option."""
@@ -107,3 +106,143 @@ class TestProjectMinimumPercent(unittest.TestCase):
             self.assertTrue(self.p._satisfies_min_translated({'completed': '0%'}))
             self.assertTrue(self.p._satisfies_min_translated({'completed': '10%'}))
             self.assertTrue(self.p._satisfies_min_translated({'completed': '90%'}))
+
+
+class TestProjectFilters(unittest.TestCase):
+    """Test filters used to decide whether to push/pull a translation or not."""
+
+    def setUp(self):
+        super(TestProjectFilters, self).setUp()
+        self.p = Project(init=False)
+        self.p.minimum_perc = None
+        self.p.resource = "resource"
+        self.stats = {
+            'en': {
+                'completed': '100%', 'last_update': '2011-11-01 15:00:00',
+            }, 'el': {
+                'completed': '60%', 'last_update': '2011-11-01 15:00:00',
+            }, 'pt': {
+                'completed': '70%', 'last_update': '2011-11-01 15:00:00',
+            },
+        }
+        self.langs = self.stats.keys()
+
+    def test_add_translation(self):
+        """Test filters for adding translations.
+
+        We do not test here for minimum percentages.
+        """
+        with patch.object(self.p, "get_resource_option") as mock:
+            mock.return_value = None
+            should_add = self.p._should_add_translation
+            for force in [True, False]:
+                for lang in self.langs:
+                    self.assertTrue(should_add(lang, self.stats, force))
+
+            # unknown language
+            self.assertTrue(should_add('es', self.stats))
+
+    def test_update_translation(self):
+        """Test filters for updating a translation.
+
+        We do not test here for minimum percentages.
+        """
+        with patch.object(self.p, "get_resource_option") as mock:
+            mock.return_value = None
+
+            should_update = self.p._should_update_translation
+            force = True
+            for lang in self.langs:
+                self.assertTrue(should_update(lang, self.stats, 'foo', force))
+
+            force = False       # reminder
+            local_file = 'foo'
+
+            # unknown language
+            self.assertTrue(should_update('es', self.stats, local_file))
+
+            # no local file
+            with patch.object(self.p, "_get_time_of_local_file") as time_mock:
+                time_mock.return_value = None
+                with patch.object(self.p, "get_full_path") as path_mock:
+                    path_mock.return_value = "foo"
+                    for lang in self.langs:
+                        self.assertTrue(
+                            should_update(lang, self.stats, local_file)
+                        )
+
+            # older local files
+            local_times = [self.p._generate_timestamp('2011-11-01 14:00:59')]
+            results = itertools.cycle(local_times)
+            def side_effect(*args):
+                return results.next()
+
+            with patch.object(self.p, "_get_time_of_local_file") as time_mock:
+                time_mock.side_effect = side_effect
+                with patch.object(self.p, "get_full_path") as path_mock:
+                    path_mock.return_value = "foo"
+                    for lang in self.langs:
+                        self.assertTrue(
+                            should_update(lang, self.stats, local_file)
+                        )
+
+            # newer local files
+            local_times = [self.p._generate_timestamp('2011-11-01 15:01:59')]
+            results = itertools.cycle(local_times)
+            def side_effect(*args):
+                return results.next()
+
+            with patch.object(self.p, "_get_time_of_local_file") as time_mock:
+                time_mock.side_effect = side_effect
+                with patch.object(self.p, "get_full_path") as path_mock:
+                    path_mock.return_value = "foo"
+                    for lang in self.langs:
+                        self.assertFalse(
+                            should_update(lang, self.stats, local_file)
+                        )
+
+    def test_push_translation(self):
+        """Test filters for pushing a translation file."""
+        with patch.object(self.p, "get_resource_option") as mock:
+            mock.return_value = None
+
+            local_file = 'foo'
+            should_push = self.p._should_push_translation
+            force = True
+            for lang in self.langs:
+                self.assertTrue(should_push(lang, self.stats, local_file, force))
+
+            force = False       # reminder
+
+            # unknown language
+            self.assertTrue(should_push('es', self.stats, local_file))
+
+            # older local files
+            local_times = [self.p._generate_timestamp('2011-11-01 14:00:59')]
+            results = itertools.cycle(local_times)
+            def side_effect(*args):
+                return results.next()
+
+            with patch.object(self.p, "_get_time_of_local_file") as time_mock:
+                time_mock.side_effect = side_effect
+                with patch.object(self.p, "get_full_path") as path_mock:
+                    path_mock.return_value = "foo"
+                    for lang in self.langs:
+                        self.assertFalse(
+                            should_push(lang, self.stats, local_file)
+                        )
+
+            # newer local files
+            local_times = [self.p._generate_timestamp('2011-11-01 15:01:59')]
+            results = itertools.cycle(local_times)
+            def side_effect(*args):
+                return results.next()
+
+            with patch.object(self.p, "_get_time_of_local_file") as time_mock:
+                time_mock.side_effect = side_effect
+                with patch.object(self.p, "get_full_path") as path_mock:
+                    path_mock.return_value = "foo"
+                    for lang in self.langs:
+                        self.assertTrue(
+                            should_push(lang, self.stats, local_file)
+                        )
