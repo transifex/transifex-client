ae9975890ef714757ad53716bb9c73ebce0deb9d
Apostolos Bessas <mpessas@indifex.com>
2011-11-04 14:14:11 +0200

    Use new API for fetching the details for a release.

--- txclib/urls.py
+++ txclib/urls.py
@@ -4,7 +4,7 @@ API_URLS = {
     'get_resources': '%(hostname)s/api/2/project/%(project)s/resources/',
     'project_details': '%(hostname)s/api/2/project/%(project)s/?details',
     'resource_details': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/',
-    'release_details': '%(hostname)s/api/project/%(project)s/release/%(release)s/',
+    'release_details': '%(hostname)s/api/2/project/%(project)s/release/%(release)s/',
     'pull_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file',
     'resource_stats': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/stats/',
     'delete_translation': '%(hostname)s/api/project/%(project)s/resource/%(resource)s/%(language)s/file/',
