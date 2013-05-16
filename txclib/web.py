5246f188b0abcc1a4c20894fcab88f7a6cd6cfd9
Apostolos Bessas <mpessas@transifex.com>
2013-05-16 15:21:41 +0300

    Fallback to distro-specific files for SSL certs
    Rely on the distributions to provide any updates.

--- txclib/web.py
+++ txclib/web.py
@@ -135,6 +135,28 @@ def certs_file():
                 f.write(content)
         return ca_file
     else:
+        POSSIBLE_CA_BUNDLE_PATHS = [
+            # Red Hat, CentOS, Fedora and friends
+            # (provided by the ca-certificates package):
+            '/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem',
+            '/etc/ssl/certs/ca-bundle.crt',
+            '/etc/pki/tls/certs/ca-bundle.crt',
+            # Ubuntu, Debian, and friends
+            # (provided by the ca-certificates package):
+            '/etc/ssl/certs/ca-certificates.crt',
+            # FreeBSD (provided by the ca_root_nss package):
+            '/usr/local/share/certs/ca-root-nss.crt',
+            # openSUSE (provided by the ca-certificates package),
+            # the 'certs' directory is the
+            # preferred way but may not be supported by the SSL module,
+            # thus it has 'ca-bundle.pem'
+            # as a fallback (which is generated from pem files in the
+            # 'certs' directory):
+            '/etc/ssl/ca-bundle.pem',
+        ]
+        for path in POSSIBLE_CA_BUNDLE_PATHS:
+            if os.path.exists(path):
+                return path
         return resource_filename(__name__, 'cacert.pem')
 
 
