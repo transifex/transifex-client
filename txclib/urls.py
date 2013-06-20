a80320735973dd608b48520bf3b89ad53e2b088b
Ilias-Dimitrios Vrachnis <vid@transifex.com>
2013-06-20 15:19:29 +0300

    Fix the translation mode.
    If --mode=translator was used, a wrong GET parameter would get
    passed to the server making the API call fail.

--- txclib/urls.py
+++ txclib/urls.py
@@ -7,7 +7,7 @@ API_URLS = {
     'release_details': '%(hostname)s/api/2/project/%(project)s/release/%(release)s/',
     'pull_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file',
     'pull_reviewed_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file&mode=reviewed',
-    'pull_translator_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file&mode=translated',
+    'pull_translator_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file&mode=translator',
     'pull_developer_file': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/translation/%(language)s/?file&mode=default',
     'resource_stats': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/stats/',
     'create_resource': '%(hostname)s/api/2/project/%(project)s/resources/',
@@ -17,5 +17,3 @@ API_URLS = {
     'formats': '%(hostname)s/api/2/formats/',
     'delete_resource': '%(hostname)s/api/2/project/%(project)s/resource/%(resource)s/',
 }
-
-
