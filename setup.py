7bfc9c232ee00b2ad5eb13b35056a6523ca8962a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-12-08 18:28:43 +0100

    Add Python 3 classifier to setup.py

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
     ],
     **extra_args
 )
