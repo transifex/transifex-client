f6956087abc9618d7adcfe8eb271ce24d9d07489
Diego Búrigo Zacarão <diegobz@transifex.com>
2015-11-05 16:57:32 -0800

    Python 3.5 supported version in setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
     ],
     **extra_args
 )
