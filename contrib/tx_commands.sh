fa2acfe8df72e9200ce2efeab19685bc22251233
Diego Búrigo Zacarão <diegobz@transifex.com>
2015-11-06 19:05:23 -0800

    Expose traceback info in CI tests

--- contrib/tx_commands.sh
+++ contrib/tx_commands.sh
@@ -7,6 +7,6 @@ cd txci
 rm -rf .tx
 $TX init --host="https://www.transifex.com" --user=$TRANSIFEX_USER --pass=$TRANSIFEX_PASSWORD
 $TX set --auto-local -r txci.$BRANCH -s en 'locale/<lang>/LC_MESSAGES/django.po' -t PO --execute
-$TX push -s
-$TX pull -l pt_BR -f
-$TX delete -f
+$TX --traceback push -s
+$TX --traceback pull -l pt_BR -f
+$TX --traceback delete -f
