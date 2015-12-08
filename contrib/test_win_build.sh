2bdef8b04dfd759d2ba7fd3a90d652c1591c31a1
Aarni Koskela <akx@iki.fi>
2015-12-08 23:42:03 +0200

    Test both the Setuptools and PyInstaller tx scripts on Windows

--- contrib/test_win_build.sh
+++ contrib/test_win_build.sh
@@ -1,4 +1,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BRANCH="win-${PYTHON//C:\\/}"
+
+# Test the PyInstaller executable:
 TX="$DIR/../dist/tx.exe"
 source "$DIR/tx_commands.sh"
+
+# Test the Setuptools script:
+TX="tx"
+source "$DIR/tx_commands.sh"
+
