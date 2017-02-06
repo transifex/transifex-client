cbb492c5ebc732d2782d967f3418f40a67afd938
Chris Mark <chrismarkou92@gmail.com>
2017-02-06 17:39:20 +0200

    comment out xliff part on bash script

--- contrib/tx_commands.sh
+++ contrib/tx_commands.sh
@@ -12,22 +12,22 @@ git clone https://github.com/transifex/txci.git
 cd txci
 rm -rf .tx
 $TX init --host="https://www.transifex.com" --user=$TRANSIFEX_USER --pass=$TRANSIFEX_PASSWORD
-$TX set --auto-local -r txci.$BRANCH\_$RANDOM -s en 'locale/<lang>/LC_MESSAGES/django.po' -t PO --execute
+$TX set --auto-local -r txci.$BRANCH -s en 'locale/<lang>/LC_MESSAGES/django.po' -t PO --execute
 
 # push/pull without XLIFF
 $TX --traceback push -s
 $TX --traceback pull -l pt_BR -f
 
-# Push dummy translation to pt_BR language
-cp locale/en/LC_MESSAGES/django.po locale/pt_BR/LC_MESSAGES/django.po
-yes | $TX --traceback push -t -l pt_BR
-
-# try to download translation xliff
-$TX --traceback pull -l pt_BR -f --xliff
-echo 'Checking if translation xlf file has been downloaded...'
-ls locale/pt_BR/LC_MESSAGES/django.po.xlf
-
-# upload xliff
-yes | $TX --traceback push -t -l pt_BR -f --xliff
+# # Push dummy translation to pt_BR language
+# cp locale/en/LC_MESSAGES/django.po locale/pt_BR/LC_MESSAGES/django.po
+# yes | $TX --traceback push -t -l pt_BR -f
+#
+# # try to download translation xliff
+# $TX --traceback pull -l pt_BR -f --xliff
+# echo 'Checking if translation xlf file has been downloaded...'
+# ls locale/pt_BR/LC_MESSAGES/django.po.xlf
+#
+# # upload xliff
+# yes | $TX --traceback push -t -l pt_BR -f --xliff
 
 $TX --traceback delete -f
