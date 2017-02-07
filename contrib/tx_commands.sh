2eede9acf57e9d235d40db3c322c0519a3f0f5f6
Chris Mark <chrismarkou92@gmail.com>
2017-02-07 12:23:53 +0200

    add random postfix on testing resource name

--- contrib/tx_commands.sh
+++ contrib/tx_commands.sh
@@ -12,7 +12,7 @@ git clone https://github.com/transifex/txci.git
 cd txci
 rm -rf .tx
 $TX init --host="https://www.transifex.com" --user=$TRANSIFEX_USER --pass=$TRANSIFEX_PASSWORD
-$TX set --auto-local -r txci.$BRANCH\_$TRANSIFEX_USER -s en 'locale/<lang>/LC_MESSAGES/django.po' -t PO --execute
+$TX set --auto-local -r txci.$BRANCH\_$TRANSIFEX_USER\_$RANDOM -s en 'locale/<lang>/LC_MESSAGES/django.po' -t PO --execute
 
 # push/pull without XLIFF
 echo "Pushing Source..."
