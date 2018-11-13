b3c2ab1a26458caff229b24393802afa7bbb357e
Rigas Papathanasopoulos <rigas@transifex.com>
2018-11-13 12:16:37 +0200

    Support Python 3.4
    Only test latest 2.X and 3.X versions in CircleCI

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license="GPLv2",
     dependency_links=[],
     setup_requires=[],
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<3.8",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<3.8",
     install_requires=get_file_content("requirements.txt").splitlines(),
     tests_require=["mock"],
     data_files=[],
