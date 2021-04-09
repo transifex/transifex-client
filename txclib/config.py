90151c262d401ce87ef856cccef1dcbd69fcc86e
Tom Klingenberg <tklingenberg@lastflood.net>
2021-04-09 10:06:18 +0200

    Minor config file newline writing
    Write extra newline in front of any section but the first one.
    
    Instead of writing an extra newline after any section.

--- txclib/config.py
+++ txclib/config.py
@@ -14,20 +14,21 @@ class OrderedRawConfigParser(configparser.RawConfigParser):
     """
     def write(self, fp):
         """Write an .ini-format representation of the configuration state."""
+        section_prefix = ''
         if self._defaults:
             fp.write("[%s]\n" % DEFAULTSECT)
             for key in sorted(self._defaults):
                 fp.write("%s = %s\n" % (key, str(self._defaults[key]).
                          replace('\n', '\n\t')))
-            fp.write("\n")
+            section_prefix = '\n'
         for section in self._sections:
-            fp.write("[%s]\n" % section)
+            fp.write("%s[%s]\n" % (section_prefix, section))
             for key in sorted(self._sections[section]):
                 if key != "__name__":
                     fp.write("%s = %s\n" %
                              (key, str(self._sections[section][key]).
                               replace('\n', '\n\t')))
-            fp.write("\n")
+            section_prefix = '\n'
 
     optionxform = str
 
